4e76487501fe92548c9ec4255e1ba38a50a1d033
David-Muhasa <davidmutsumbirwa@gmail.com>
2023-05-05 07:23:40 +0200

    presque fini

--- cours_web2/classEmploye.php
+++ cours_web2/classEmploye.php
@@ -7,15 +7,21 @@ class Employe
     protected $sexe;
     protected $post;
     protected $etatCivil;
-    protected $retenus;
+    protected $retunus;
+
+    const MARIE = 10;
+    const SPECIALISTE = 100;
+    const LOGEMENT = 20;
+    const GENRE = 5;
+
 
     public function __construct(
         string $nom,
         string $prenom,
         float $salaire = 100.00,
-        string $sexe,
-        string $post,
-        string $etatCivil,
+        string $sexe = 'm',
+        string $post = 'enseignant',
+        string $etatCivil = 'marie',
         float $retenus = 0.00
     ) {
         $this->nom = $nom;
@@ -24,7 +30,7 @@ class Employe
         $this->sexe = $sexe;
         $this->post = $post;
         $this->etatCivil = $etatCivil;
-        $this->retenus = $retenus;
+        $this->retunus = $retenus;
     }
 
     public function getNom()
@@ -50,5 +56,50 @@ class Employe
     public function getEtatCivil()
     {
         return $this->etatCivil;
+    }
+
+    public function getRetunus()
+    {
+        return $this->retunus;
+    }
+
+
+    public function setNom($nom)
+    {
+        return $this->nom = $nom;
+    }
+    public function setPostnom($prenom)
+    {
+        return $this->prenom = $prenom;
+    }
+    public function setSalaire($salaire)
+    {
+        return $this->salaire = $salaire;
+    }
+    public function setSexe($sexe)
+    {
+        return $this->sexe = $sexe;
+    }
+    public function setPost($post)
+    {
+        return $this->post = $post;
+    }
+    public function setEtatcivil($etatCivil)
+    {
+        return $this->etatCivil = $etatCivil;
+    }
+
+    public function  exec()
+    {
+
+        $calcul = $this->salaire + self::LOGEMENT;
+
+        if ($this->etatCivil == 'marie') {
+            $calcul = $calcul + self::MARIE;
+        }
+
+
+
+        return $calcul - $this->retunus;
     }
 }
