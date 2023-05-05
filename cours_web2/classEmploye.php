2074e0bbb0b9b82b5c09d612b696f4df5f1b6f70
David-Muhasa <davidmutsumbirwa@gmail.com>
2023-05-05 06:39:39 +0200

    initialisation du tp

--- cours_web2/classEmploye.php
+++ cours_web2/classEmploye.php
@@ -1,8 +1,55 @@
 <?php
-    class Employe {
-        public $n_employe;
-        public $pn_employe;
-        public $salaire; 
+class Employe
+{
+
+    protected $nom;
+    protected $prenom;
+    protected $salaire;
+    protected $sexe;
+    protected $post;
+    protected $etatCivil;
+    protected $retunus;
+
+    public function __construct(
+        string $nom,
+        string $prenom,
+        float $salaire = 100.00,
+        string $sexe,
+        string $post,
+        string $etatCivil,
+        float $retenus = 0.00
+    ) {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+        $this->salaire = $salaire;
+        $this->sexe = $sexe;
+        $this->post = $post;
+        $this->etatCivil = $etatCivil;
+        $this->retunus = $retenus;
     }
-    
-?>
+
+    public function getNom()
+    {
+        return $this->nom;
+    }
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+    public function getSalaire()
+    {
+        return $this->salaire;
+    }
+    public function getSexe()
+    {
+        return $this->sexe;
+    }
+    public function getPost()
+    {
+        return $this->post;
+    }
+    public function getEtatCivil()
+    {
+        return $this->etatCivil;
+    }
+}
